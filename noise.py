03bd65a2ebabcf5a7f5e8ab882351b60d31a8e64
kevinferg <kevinferg98@gmail.com>
2024-09-12 12:38:38 -0400

    Perlin noise module and smoothness setting

--- noise.py
+++ noise.py
@@ -3,15 +3,33 @@ import torch.nn.functional as F
 
 
 class ValueNoise(torch.nn.Module):
-    def __init__(self, n_dims, n_fields, res, seed=0, periodic=False):
+    def __init__(self, n_dims, n_fields, res, seed=None, periodic=False, smoothness=1):
         super().__init__()
+        self.smoothness = int(smoothness)
+        assert self.smoothness > 0 and self.smoothness < 4, "Smoothness must be 1, 2, or 3."
+        self.wfun = self.init_wfun()
         self.n_dims = n_dims
         self.n_fields = n_fields
         self.res = res
-        self.seed = seed
+        if seed is not None:
+            self.seed = seed
+        else:
+            self.seed = torch.seed()
         self.periodic = periodic
         self.values = self.init_values()
         self.corners = self.init_corners()
+        self.name = "Value Noise"
+
+    def init_wfun(self):
+        if self.smoothness == 1:
+            # Linear interpolation AKA linstep
+            return lambda w: w
+        elif self.smoothness == 2:
+            # Cubic Hermite interpolation AKA smoothstep
+            return lambda w: (3. - 2.*w)*w*w
+        elif self.smoothness == 3:
+            # Smootherstep
+            return lambda w: (10. + w*(6.*w - 15.))*w*w*w
 
     def init_corners(self):
         zero_one = torch.tensor([0,1],dtype=torch.long)
@@ -42,34 +60,73 @@ class ValueNoise(torch.nn.Module):
 
         for i in range(self.n_dims):
             weights = locs[:,i, *(None,)*(self.n_dims - i)]
-            vals = torch.lerp(vals[:,0,...], vals[:,1,...], weights)
+            vals = torch.lerp(vals[:,0,...], vals[:,1,...], self.wfun(weights))
 
         return vals
 
 
-
-
-
-
-
-
-
-
 class PerlinNoise(torch.nn.Module):
-    def __init__(self, n_dims, n_fields, res, seed=0):
+    def __init__(self, n_dims, n_fields, res, seed=None, periodic=False, smoothness=1):
         super().__init__()
+        self.smoothness = int(smoothness)
+        assert self.smoothness > 0 and self.smoothness < 4, "Smoothness must be 1, 2, or 3."
+        self.wfun = self.init_wfun()
         self.n_dims = n_dims
         self.n_fields = n_fields
         self.res = res
-        self.seed = seed
-        self.gradients = self.init_gradients()
-        self.verts = self.init_verts()
+        if seed is not None:
+            self.seed = seed
+        else:
+            self.seed = torch.seed()
+        self.periodic = periodic
+        self.grads = self.init_grads()
+        self.corners = self.init_corners()
+        self.name = "Perlin Noise"
 
-    def init_gradients(self):
+    def init_wfun(self):
+        if self.smoothness == 1:
+            # Linear interpolation AKA linstep
+            return lambda w: w
+        elif self.smoothness == 2:
+            # Cubic Hermite interpolation AKA smoothstep
+            return lambda w: (3. - 2.*w)*w*w
+        elif self.smoothness == 3:
+            # Smootherstep
+            return lambda w: (10. + w*(6.*w - 15.))*w*w*w
+
+    def init_corners(self):
+        zero_one = torch.tensor([0,1],dtype=torch.long)
+        return torch.meshgrid((zero_one,)*self.n_dims, indexing='ij')
+
+    def init_grads(self):
         torch.manual_seed(self.seed)
-        shape = (self.res, self.res, self.n_dims, self.n_fields)
+        shape = (*([self.res+1,]*self.n_dims), self.n_fields, self.n_dims)
         g = torch.normal(torch.zeros(*shape), torch.ones(*shape))
-        return F.normalize(g, dim=2)
-    
+        return F.normalize(g, dim=-1)
+
     def forward(self, x):
-        return NotImplementedError
+        # x is shape (Npts, dims)
+        x = torch.fmod(x*self.res, self.res) # Wrap points
+
+        locs = torch.frac(x)              # Local coordinates of x
+        idxA = torch.floor(x).long()      # indices of vertices below x
+        idxB = idxA + 1                   # indices of vertices above x
+        if self.periodic:
+            idxB[idxB == self.res] = 0    # -> wrap for periodic boundary
+        idx = torch.stack((idxA, idxB), dim=-1)
+        offsets = torch.stack((locs, 1.0-locs), dim=-1)
+
+        idx_verts = []
+        offsets_verts = []
+        for i in range(self.n_dims):
+            idx_verts.append(idx[:,i,self.corners[i]])
+            offsets_verts.append(offsets[:,i,self.corners[i]])
+        idx_verts = torch.stack(idx_verts, dim=-1)
+        offsets_verts = torch.stack(offsets_verts, dim=-1).unsqueeze(-2)
+        grads = self.grads[*[idx_verts[...,i] for i in range(self.n_dims)], ...]
+
+        vals = torch.sum(grads * offsets_verts, dim=-1)
+        for i in range(self.n_dims):
+            weights = locs[:,i, *(None,)*(self.n_dims - i)]
+            vals = torch.lerp(vals[:,0,...], vals[:,1,...], self.wfun(weights))
+        return vals
